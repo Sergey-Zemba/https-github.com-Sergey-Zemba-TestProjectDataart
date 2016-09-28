4c16e34a46afad7fc1a8857a822d45c29fc8fa69
szemba <szemba@SZEMBA>
2016-09-28 20:00:11 +0300

    the end of the day

--- DataBase/SPs/filling_table.sql
+++
@@ -1,6 +0,0 @@
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[filling_table]') AND type in (N'P', N'PC'))
-AND EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='News')
-CREATE PROCEDURE [dbo].[filling_table] AS
-BEGIN
-EXEC create_proc;
-END;
