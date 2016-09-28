7ab1af60a4848a2d8af47230c054b1fac19cfd88
Sergey-Zemba <sergey.zemba@gmail.com>
2016-09-28 18:47:24 +0300

    Update news_filling.sql

--- DataBase/SPs/news_filling.sql
+++ DataBase/SPs/news_filling.sql
@@ -1,7 +1,9 @@
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[news_filling]') AND type in (N'P', N'PC'))
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[news_filling]') AND type in (N'P', N'PC'))
 AND EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='dbo' AND TABLE_NAME='News')
-EXEC(
-'CREATE PROCEDURE news_filling AS
+DROP PROCEDURE news_filling;
+
+CREATE PROCEDURE news_filling AS
+
 BEGIN
 DECLARE @Cur INT = 1;
 WHILE(@Cur<=100)
@@ -22,6 +24,5 @@ GETDATE()
 )
 SET @Cur = @Cur + 1;
 END
-END;'
-)
+END;
 EXEC news_filling;
