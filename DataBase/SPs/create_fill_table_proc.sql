8df5d616f1e09f476cd598285857f619d279f2fa
szemba <szemba@SZEMBA>
2016-09-29 11:35:06 +0300

    deleted fullstop

--- DataBase/SPs/create_fill_table_proc.sql
+++ DataBase/SPs/create_fill_table_proc.sql
@@ -1,4 +1,4 @@
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'fill_table_proc') AND type in (N'P', N'PC'))
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE object_id = OBJECT_ID(N'fill_table_proc') AND type in (N'P', N'PC'))
 BEGIN
 CREATE PROCEDURE fill_table_proc AS
 BEGIN
