e15323d9b1037b39c96e47636f9435a4503c56e6
szemba <szemba@SZEMBA>
2016-09-29 13:20:46 +0300

    Added CreateTableTAsk

---
+++ MyTasks/CreateTableTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace MyTasks
+{
+    public class CreateTableTask : Task
+    {
+        public string ConnectionString { get; set; }
+        public ITaskItem CreateTableScript { get; set; }
+
+        public override bool Execute()
+        {
+            string sql = SqlExecutor.ReadFile(CreateTableScript.ItemSpec);
+            bool result = SqlExecutor.ExecuteSql(sql, ConnectionString);
+            if (!(result || BuildEngine.ContinueOnError))
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
