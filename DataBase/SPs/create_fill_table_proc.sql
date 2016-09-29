1785fcfd1a3f015aecb8618f0cfbcaa6db4386b3
szemba <szemba@SZEMBA>
2016-09-29 11:43:14 +0300

    removed crap

--- DataBase/SPs/create_fill_table_proc.sql
+++ DataBase/SPs/create_fill_table_proc.sql
@@ -23,8 +23,3 @@ SET @Cur = @Cur + 1;
 END
 END
 END;
-
-IF EXISTS ( SELECT * 
-            FROM   sysobjects 
-            WHERE  id = object_id(N'[dbo].[MyProc]') 
-                   and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
