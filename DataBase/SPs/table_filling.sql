d8258c09aab03c122e8a8eeaac9ee2edd19badd2
szemba <szemba@SZEMBA>
2016-09-29 17:16:47 +0300

    changed drop

--- DataBase/SPs/table_filling.sql
+++ DataBase/SPs/table_filling.sql
@@ -1,6 +1,6 @@
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'fill_table_proc') AND type IN ( N'P', N'PC' ))
 BEGIN
-EXECUTE DROP PROCEDURE fill_table_proc
+EXECUTE ('DROP PROCEDURE fill_table_proc')
 EXECUTE ('CREATE PROCEDURE fill_table_proc AS
 BEGIN
 DECLARE @Cur INT = 1;
