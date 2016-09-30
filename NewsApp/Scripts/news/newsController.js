a37a899b87b6f38c8b527451b4d8cce9aeb52fdf
szemba <szemba@SZEMBA>
2016-09-30 14:35:47 +0300

    resolved problem with pagination

--- NewsApp/Scripts/news/newsController.js
+++ NewsApp/Scripts/news/newsController.js
@@ -1,31 +1,36 @@
 ﻿angular.module("newsApp").controller("newsController",
-    function ($scope, $http, $cookies) {
+    function ($scope, $http, $cookies, $window) {
         $scope.news = {};
         $scope.currentPage = 1;
-        //if ($cookies.get("currentPage")) {
-        //    $scope.currentPage = parseInt($cookies.get("currentPage"));
-        //    $cookies.remove("currentPage");
-        //}
         $scope.totalItems = {};
         $scope.maxSize = 5;
         $scope.getArticlesUrl = {};
-        $scope.setLane = function (data) {
+        $scope.setLane = function (data, page) {
             $scope.totalItems = data.articlesNum;
             $scope.news = {
                 articles: data.articles
             };
             $scope.isAuthenticated = data.isAuthenticated;
             $(document).scrollTop(0);
+            $scope.currentPage = page;
         };
         $scope.getArticles = function (page, callback) {
+            var goToPage = parseInt($cookies.get("currentPage"));
+            if (goToPage) {
+                page = goToPage;
+                $cookies.remove("currentPage");
+            }
             $http.get(getArticlesUrl + "/" + page).success(function (data) {
-                callback(data);
+                callback(data, page);
             });
         }
         $scope.delete = function (id) {
             $http.get(deleteUrl + '/' + id).success(function (data) {
                 $scope.getArticles($scope.currentPage, $scope.setLane);
             });
+        }
+        $scope.savePage = function () {
+            $cookies.put("currentPage", $scope.currentPage);
         }
         $scope.getArticles($scope.currentPage, $scope.setLane);
     });
