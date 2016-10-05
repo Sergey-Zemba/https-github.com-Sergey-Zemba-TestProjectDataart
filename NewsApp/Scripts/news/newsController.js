4ce4dfcdc1e4d96a369eaefe7581755a84cf30fa
szemba <szemba@SZEMBA>
2016-10-05 13:10:24 +0300

    corrected ng-init

--- NewsApp/Scripts/news/newsController.js
+++ NewsApp/Scripts/news/newsController.js
@@ -1,6 +1,15 @@
 ﻿newsApp.controller("NewsController", ['$scope', 'newsService', 'pageService', function ($scope, newsService, pageService) {
     $scope.currentPage = 1;
     $scope.maxSize = 5;
+    $scope.delete = function (id) {
+        newsService.deleteArticle(deleteUrl, id)
+            .then(function(result) {
+                $scope.getArticles($scope.currentPage);
+            });
+    }
+    $scope.savePage = function () {
+        pageService.savePage($scope.currentPage);
+    }
     $scope.getArticles = function (page) {
         page = pageService.getPage(page);
         newsService.getContent(getArticlesUrl, page).then(function (result) {
@@ -12,14 +21,5 @@
             $scope.currentPage = page;
         });
         $(document).scrollTop(0);
-    }
-    $scope.delete = function (id) {
-        newsService.deleteArticle(deleteUrl, id)
-            .then(function(result) {
-                $scope.getArticles($scope.currentPage);
-            });
-    }
-    $scope.savePage = function () {
-        pageService.savePage($scope.currentPage);
     }
 }]);
