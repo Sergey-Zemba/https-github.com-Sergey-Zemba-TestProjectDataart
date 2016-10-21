4a576a23cf7bb35315a73a73f7643205bcfb4884
szemba <szemba@SZEMBA>
2016-10-21 11:53:34 +0300

    rollback

--- DataBase/SPs/table_filling.sql
+++ DataBase/SPs/table_filling.sql
@@ -1,9 +1,6 @@
 IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'fill_table_proc') AND type IN ( N'P', N'PC' ))
-BEGIN
-DROP PROCEDURE fill_table_proc
-END
-BEGIN
-CREATE PROCEDURE fill_table_proc AS
+EXECUTE ('DROP PROCEDURE fill_table_proc')
+EXECUTE ('CREATE PROCEDURE fill_table_proc AS
 BEGIN
 DECLARE @Cur INT = 1;
 WHILE(@Cur<=100)
@@ -16,16 +13,15 @@ ImageName,
 CreationDate
 )
 VALUES(
-'Title' + CAST(@Cur AS NVARCHAR(10)),
-'ShortDescriiption' + CAST(@Cur AS NVARCHAR(10)),
-'Content' + CAST(@Cur AS NVARCHAR(10)),
-'ImageName' + CAST(@Cur AS NVARCHAR(10)),
+''Title'' + CAST(@Cur AS NVARCHAR(10)),
+''ShortDescriiption'' + CAST(@Cur AS NVARCHAR(10)),
+''Content'' + CAST(@Cur AS NVARCHAR(10)),
+''ImageName'' + CAST(@Cur AS NVARCHAR(10)),
 GETDATE()
 );
 SET @Cur = @Cur + 1;
 END
-END
-END
+END')
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA ='dbo' AND TABLE_NAME = 'News')
 AND NOT EXISTS (SELECT 1 from News)
 BEGIN
