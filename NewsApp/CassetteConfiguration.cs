987ee8b5995c1ade040507d1f39b3fd7b15ef0c3
szemba <szemba@SZEMBA>
2016-10-25 11:58:14 +0300

    configured cassette for all scripts and styles

--- NewsApp/CassetteConfiguration.cs
+++ NewsApp/CassetteConfiguration.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Cassette;
 using Cassette.Scripts;
 using Cassette.Stylesheets;
@@ -11,29 +12,15 @@ namespace NewsApp
     {
         public void Configure(BundleCollection bundles)
         {
-            // TODO: Configure your bundles here...
-            // Please read http://getcassette.net/documentation/configuration
-
-            // This default configuration treats each file as a separate 'bundle'.
-            // In production the content will be minified, but the files are not combined.
-            // So you probably want to tweak these defaults!
+            List<string> layoutScripts = new List<string>();
+            layoutScripts.Add("Scripts/modernizr-2.6.2.js");
+            layoutScripts.Add("Scripts/jquery-1.10.2.js");
+            layoutScripts.Add("Scripts/bootstrap.js");
+            layoutScripts.Add("Scripts/angular.min.js");
+            bundles.Add<ScriptBundle>("LayoutScripts", layoutScripts);
             bundles.Add<ScriptBundle>("Scripts/news");
-            //bundles.Add<ScriptBundle>("~/bundles/layout",
-            //                            new[]
-            //                            {
-            //                                "~/Scripts/modernizr-2.6.2.js",
-            //                                "~/Scripts/jquery-1.10.2.js",
-            //                                "~/Scripts/bootstrap.js",
-            //                                "~/Scripts/angular.min.js"
-            //                            });
-            // To combine files, try something like this instead:
-            //   bundles.Add<StylesheetBundle>("Content");
-            // In production mode, all of ~/Content will be combined into a single bundle.
-
-            // If you want a bundle per folder, try this:
-            //   bundles.AddPerSubDirectory<ScriptBundle>("Scripts");
-            // Each immediate sub-directory of ~/Scripts will be combined into its own bundle.
-            // This is useful when there are lots of scripts for different areas of the website.
+            bundles.Add<StylesheetBundle>("LayoutStyles", new List<string>() {"Content/bootstrap.css", "Content/Site.css"});
+            bundles.Add<StylesheetBundle>("IndexStyles", new List<string>() {"Content/index.css"});
         }
     }
 }
