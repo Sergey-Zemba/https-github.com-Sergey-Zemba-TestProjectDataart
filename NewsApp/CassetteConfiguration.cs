d90e1ca62e17ee6ea23f55f5ce58ec2f07c58696
szemba <szemba@SZEMBA>
2016-10-26 15:27:48 +0300

    removed bower-task, added gulpbower, downloading bower packages to scripts folder, changed casseteConfiguration

--- NewsApp/CassetteConfiguration.cs
+++ NewsApp/CassetteConfiguration.cs
@@ -13,19 +13,19 @@ namespace NewsApp
         public void Configure(BundleCollection bundles)
         {
             List<string> layoutScripts = new List<string>();
-            layoutScripts.Add("Scripts/modernizr-2.6.2.js");
-            layoutScripts.Add("bower_components/jquery/dist/jquery.js");
-            layoutScripts.Add("bower_components/bootstrap/dist/js/bootstrap.js");
-            layoutScripts.Add("bower_components/angular/angular.js");
+            layoutScripts.Add("scripts/modernizr-2.6.2.js");
+            layoutScripts.Add("scripts/jquery/dist/jquery.js");
+            layoutScripts.Add("scripts/bootstrap/dist/js/bootstrap.js");
+            layoutScripts.Add("bowerscripts_components/angular/angular.js");
             bundles.Add<ScriptBundle>("LayoutScripts", layoutScripts);
             bundles.Add<ScriptBundle>("Scripts/news");
             List<string> angularTools = new List<string>();
-            angularTools.Add("bower_components/angular-animate/angular-animate.js");
-            angularTools.Add("bower_components/angular-sanitize/angular-sanitize.js");
-            angularTools.Add("bower_components/angular-cookies/angular-cookies.js");
-            angularTools.Add("bower_components/angular-bootstrap/ui-bootstrap-tpls.js");
+            angularTools.Add("scripts/angular-animate/angular-animate.js");
+            angularTools.Add("scripts/angular-sanitize/angular-sanitize.js");
+            angularTools.Add("scripts/angular-cookies/angular-cookies.js");
+            angularTools.Add("scripts/angular-bootstrap/ui-bootstrap-tpls.js");
             bundles.Add<ScriptBundle>("AngularTools", angularTools);
-            bundles.Add<StylesheetBundle>("LayoutStyles", new List<string>() { "bower_components/bootstrap/dist/css/bootstrap.css", "Content/Site.css"});
+            bundles.Add<StylesheetBundle>("LayoutStyles", new List<string>() { "scripts/bootstrap/dist/css/bootstrap.css", "Content/Site.css"});
             bundles.Add<StylesheetBundle>("IndexStyles", new List<string>() {"Content/index.css"});
         }
     }
