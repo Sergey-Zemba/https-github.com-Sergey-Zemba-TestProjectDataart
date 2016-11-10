c753f6477b5a35299e10d8e701ffda0f71211eb2
szemba <szemba@SZEMBA>
2016-11-10 10:35:02 +0200

    Added test project with a simole test

---
+++ NewsApp.Tests/HomeControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewsApp.Controllers;
+
+namespace NewsApp.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController controller = new HomeController();
+            ViewResult result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
